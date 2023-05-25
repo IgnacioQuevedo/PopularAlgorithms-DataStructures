9dadaccb1b63c63444d227e7700ef01bc3932707
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2023-05-25 10:59:34 -0300

    Mejoramos Heap

--- TADS/TadHeap.cpp
+++ TADS/TadHeap.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
 template <class T>
 class nodoHeap
 {
@@ -29,13 +33,15 @@ private:
     void flotar(int pos)
     {
 
-        if (pos == 0 || this->vecNodoHeap[pos] > this->vecNodoHeap[(pos - 1) / 2])
+        int pepe = this->vecNodoHeap[pos]->prioridad;
+        int pepe2 = this->vecNodoHeap[(pos-1)/2]->prioridad;
+        if (pos == 0 || (this->vecNodoHeap[pos]->prioridad) > (this->vecNodoHeap[(pos - 1) / 2]->prioridad))
         { // Si el hijo es mas grande esta Ok.
             return;
         }
         else // Sino hago Swap
         {                                                              
-            int aux = this->vecNodoHeap[(pos - 1) / 2];                // Me guardo el padre
+            nodoHeap<T> *aux = this->vecNodoHeap[(pos - 1) / 2];                // Me guardo el padre
             this->vecNodoHeap[(pos - 1) / 2] = this->vecNodoHeap[pos]; // el padre es el nuevo hijo
             this->vecNodoHeap[pos] = aux;                              // el hijo pasa a ser el padre
             flotar((pos - 1) / 2);                                     // Repito con el nuevo padre.
@@ -77,7 +83,7 @@ private:
                 if (this->vecNodoHeap[pos]->prioridad > this->vecNodoHeap[pos * 2 + 1]->prioridad) // EN CASO QUE MI PADRE ES MAS GRANDE QUE MI UNICO HIJO (IZQ)
                 {
                     // SWAP
-                    int aux = this->vecNodoHeap[pos];
+                    nodoHeap<T>* aux = this->vecNodoHeap[pos];
                     this->vecNodoHeap[pos] = this->vecNodoHeap[2 * pos + 1];
                     this->vecNodoHeap[2 * pos + 1] = aux;
                 }
@@ -88,16 +94,17 @@ private:
             }
             else
             { // ES PORQUE TIENE HIJO DER Y IZQ -> ME FIJO CUAL ES MAS CHICO PARA EL SWAP
-
+                if(2 * pos + 1 >= this->tope && 2 * pos + 2 >= this-> tope){
+                    return;
+                }
                 int posHijoChico = this->min(2 * pos + 1, 2 * pos + 2); // Me guardo la pos del de prio mas chico
                 if (this->vecNodoHeap[pos]->prioridad > this->vecNodoHeap[posHijoChico]->prioridad)
                 {
 
-                    nodoHeap aux = new nodoHeap<T>(); // checkear si anda
-                    aux = this->vecNodoHeap[pos];
-                    this->vecNodoHeap[pos] = this->vecNodoHeap[posHijoChico];
-                    this->vecNodoHeap[posHijoChico] = aux;
-                    delete aux;
+                    nodoHeap<T>* aux = new nodoHeap<T>(this->vecNodoHeap[posHijoChico]->prioridad,this->vecNodoHeap[posHijoChico]->dato);
+                    this->vecNodoHeap[posHijoChico]->prioridad = this->vecNodoHeap[pos]->prioridad;
+                    this->vecNodoHeap[posHijoChico]->dato = this->vecNodoHeap[pos]->dato; 
+                    this->vecNodoHeap[pos] = aux;
                     hundir(posHijoChico); //LLAMAS A LA RECURSION 
                 }
                 else
@@ -114,14 +121,19 @@ public:
     {
         this->maxElem = maxElem;
         this->tope = 0;
-        this->vecNodoHeap = new vecNodoHeap<T>[maxElem];
+        this->vecNodoHeap = new nodoHeap<T>*[maxElem];
+
+        for(int i=0; i<= maxElem; i++){
+            this->vecNodoHeap[i] = NULL;
+        }
+
     }
 
     void encolar(int prioridad, T dato)
     {
         if (this->tope <= this->maxElem)
         {
-            this->vecNodoHeap[tope] = new nodoHeap<T>(prioridad, dato); // Te tenes que preguntar, donde añado, y eso es en tope, q seria la pos siguiente al ultimo eleme
+            this->vecNodoHeap[tope] = new nodoHeap<T>(prioridad, dato); //TOPE = LA POS siguiente al ultimo eleme
             flotar(this->tope);
             this->tope = tope + 1; // El tope aumenta porque añadi un elemento
         }
@@ -132,10 +144,11 @@ public:
         if(tope == 0){
             return;
         }
-
+        nodoHeap<T>* aux = new nodoHeap<T> (this->vecNodoHeap[tope-1]->prioridad,this->vecNodoHeap[tope-1]->dato);
         delete this->vecNodoHeap[0];
-        this->vecNodoHeap[0] = this->vecNodoHeap[tope]; //NO SERIA TOPE-1?? VER CON GONCHI
-        delete this->vecNodoHeap[tope];
+        this->vecNodoHeap[0] = aux;
+        delete this->vecNodoHeap[tope-1];
+        this->vecNodoHeap[tope-1] = NULL;
         this->tope--;
         hundir(0);
     }
@@ -158,7 +171,7 @@ public:
 
     bool esVacia()
     {
-        return this->tope == 0:
+        return this->tope == 0;
     }
 
     int largo()
@@ -166,3 +179,15 @@ public:
         return this->tope;
     }
 };
+
+int main(int argc, char const *argv[])
+{
+    Heap<char> *miHeap = new Heap<char>(5);
+    miHeap->encolar(6,'G');
+    miHeap->encolar(0,'O');
+    miHeap->encolar(1,'I');
+    miHeap->encolar(5,'S');
+    miHeap->encolar(9,'P');
+    miHeap->desencolar();
+    return 0;
+}
